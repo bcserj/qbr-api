8c3d4325f9bb0832a9fb2a71987fa85d85b09537
bc.serj <bc.serj@gmail.com>
2022-05-26 03:28:50 +0300

    readmye

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -288,7 +288,7 @@ return [
          * Constants which can be used in annotations
          */
         'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('APP_URL', 'http://my-default-host.com'). '/api/v1',
+            'L5_SWAGGER_CONST_HOST' => env('APP_URL', 'http://my-default-host.com') . env('API_PATH', '/api/v1'),
         ],
     ],
 ];
